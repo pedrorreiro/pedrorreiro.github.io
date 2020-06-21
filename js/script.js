d88d5f983b036c9823de0f506ee4524747810a63
pedrorreiro <pedro_rique17@hotmail.com>
2020-06-20 21:50:56 -0300

    menu celular

--- js/script.js
+++ js/script.js
@@ -6,5 +6,12 @@ function mostraMenu(){
 
     else document.getElementById('itensMenuDevice').style.display = "block"
 
+}
 
+function insta(){
+    window.open('http://www.instagram.com/pedro_riqe', '_blank');
+}
+
+function facebook(){
+    window.open('http://www.facebook.com/PedroHenAlves', '_blank');
 }
