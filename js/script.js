b2cb544dd35c243ad203570d045dc9dbfe2b640d
Pedro Henrique <48803949+pedrorreiro@users.noreply.github.com>
2020-07-17 16:17:00 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+window.onresize = function(){
+
+    if(screen.width <= 700){
+        document.getElementById('iframeCookie').src = "https://pedrorreiro.github.io/cookieClicker/index-mobile";
+        console.log('menor');
+    }
+    
+    else{
+        document.getElementById('iframeCookie').src = 'https://pedrorreiro.github.io/cookieClicker/';
+    }
+
+    //document.getElementById('cookie').style.width = alturaCel/4 + 'px';
+
+}
+
+
 function mostraMenu(){
 
     if(document.getElementById('itensMenuDevice').style.display == "block"){
